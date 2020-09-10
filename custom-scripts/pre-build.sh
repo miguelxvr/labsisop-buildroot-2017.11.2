a481cf198cb0e3159a8037fc3ea00a31b9332e0f
Ubuntu <ubuntu@ip-172-31-65-239.ec2.internal>
2020-09-10 11:56:39 +0000

    added network support

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,9 +1,14 @@
 #Compile the syscall_test.c
-BUILDROOT_DIR=$BASE_DIR/..
-COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
-$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
+#BUILDROOT_DIR=$BASE_DIR/..
+#COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
+#$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
 
 
- #Install hello world driver
-make -C $BASE_DIR/../modules/hello/
-make -C $BASE_DIR/../modules/simple_driver/
+#Install hello world driver
+#make -C $BASE_DIR/../modules/hello/
+#make -C $BASE_DIR/../modules/simple_driver/
+
+#!/bin/sh
+
+cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
+chmod +x $BASE_DIR/target/etc/init.d/S41network-config
